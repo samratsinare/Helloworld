f056313dc5a09a8cd59d186399a31bee717d9353
Admin <Admin@LAPTOP-Q9PD58U4>
2019-07-06 20:41:56 +0530

    i ll changed

--- Scecond/src/Akki/Test.java
+++ Scecond/src/Akki/Test.java
@@ -4,7 +4,14 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+String s1="this is java";
+	
+	String s2="is";
+	
+	System.out.println(s1.replace(s2, " "));
+	
+	
+	
 	}
 
 }
